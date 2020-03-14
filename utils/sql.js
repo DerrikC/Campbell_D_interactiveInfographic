11fac5b8d4c51030cda3ab306ef4cb90db427535
Derrik Campbell <derrikcampbell@hotmail.com>
2020-03-14 03:45:21 -0400

    fixed heroku crash by creating pool

--- utils/sql.js
+++ utils/sql.js
@@ -1,12 +1,30 @@
-const config = require("../config");
-const sql = require('mysql');
+// const config = require("../config");
+// const sql = require('mysql');
 
-const connect = sql.createConnection({
-    host: config.host,
-    port: config.port,
-    user: config.uname,
-    password: config.pword,
-    database: config.database
+// const connect = sql.createConnection({
+//     host: config.host,
+//     port: config.port,
+//     user: config.uname,
+//     password: config.pword,
+//     database: config.database
+// });
+
+// module.exports = connect;
+
+//fix heroku crashing 
+
+var config = require('../config');
+var mysql = require('mysql');
+
+var connect = mysql.createPool({
+  host: config.host,
+  port: config.port,
+  user: config.uname,
+  password: config.pword,
+  database: config.database,
+  connectionLimit : 20,
+  queueLimit : 100,
+  waitForConnections : true
 });
 
 module.exports = connect;
