22e29b19be943980071686e50596c272ada83e04
Derrik Campbell <derrikcampbell@hotmail.com>
2020-03-14 02:28:11 -0400

    added heroku database setup

--- config.js
+++ config.js
@@ -1,8 +1,20 @@
+// const config = {
+//     host: "localhost",
+//     port: 8889, // 8889 for mac
+//     uname: "root",
+//     pword: "root", // leave blank for windows
+//     database: "db_infograph" //change if needed!!!!!!!
+// }
+
+// module.exports = config;
+
+//heroku setup
+
 const config = {
-    host: "localhost",
+    host: "us-cdbr-iron-east-04.cleardb.net",
     port: 8889, // 8889 for mac
-    uname: "root",
-    pword: "root", // leave blank for windows
+    uname: "bb9ea0c3e1d409",
+    pword: "dd1a7b03", // leave blank for windows
     database: "db_infograph" //change if needed!!!!!!!
 }
 
